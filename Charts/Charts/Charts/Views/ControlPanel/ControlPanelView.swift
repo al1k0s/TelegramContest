ee4ccccf08ef37c2e792f74595bac1a9d14d08f8
Oleksij <andaleksei@gmail.com>
2019-03-12 22:07:19 +0200

    add bottom panel view

---
+++ Charts/Charts/Charts/Views/ControlPanel/ControlPanelView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlPanelView.swift
+//  Charts
+//
+//  Created by Oleksii Andriushchenko on 3/12/19.
+//  Copyright © 2019 Алексей Андрющенко. All rights reserved.
+//
+
+import UIKit
+
+final class ControlPanelView: UIView {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .clear
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setup() {
+    
+  }
+
+}
