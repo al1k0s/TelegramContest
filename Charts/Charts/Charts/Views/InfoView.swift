3f774b1aa3c43c3ee0cd5e9066be75586f2af6db
Alik Vovkotrub <alikvovk@icloud.com>
2019-03-24 17:22:47 +0200

    Fixed circles being drawn above info view

--- Charts/Charts/Charts/Views/InfoView.swift
+++ Charts/Charts/Charts/Views/InfoView.swift
@@ -124,6 +124,7 @@ class InfoView: UIView {
       line.bottomAnchor.constraint(equalTo: self.bottomAnchor),
       lineLeftConstraint!
     ])
+    sendSubviewToBack(line)
   }
 
   required init?(coder aDecoder: NSCoder) {
@@ -168,7 +169,7 @@ class InfoView: UIView {
                                                 UIColor(red: 245/255, green: 250/255, blue: 245/255, alpha: 1) :
                                                UIColor(red: 27/255, green: 40/255, blue: 54/255, alpha: 1)
       }
-      self.addSubview(circle)
+      insertSubview(circle, aboveSubview: line)
       circles.append(circle)
     }
 
@@ -198,7 +199,9 @@ class InfoView: UIView {
   }
 
   func rangeChanged() {
-    subviews.forEach({ $0.alpha = 0 })
+    UIView.animate(withDuration: 0.1) {
+      self.subviews.forEach({ $0.alpha = 0 })
+    }
     resetCircles()
     lastViewModel = nil
   }
