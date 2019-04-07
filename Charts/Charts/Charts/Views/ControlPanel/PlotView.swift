237acaf082337f2ed99a1e83ca6634f30e647bb0
Alik Vovkotrub <alikvovk@icloud.com>
2019-04-07 17:38:43 +0300

    PlotView refactored

--- Charts/Charts/Charts/Views/ControlPanel/PlotView.swift
+++ Charts/Charts/Charts/Views/ControlPanel/PlotView.swift
@@ -9,50 +9,49 @@
 import UIKit
 
 final class PlotView: UIView {
-
+  
   private let isMainPlot: Bool
   private var chartRange: ChartRange?
-  private var boundForChange: (Double, Double) = (-1, -1)
-  private var boundForCheck: (Double, Double) = (-1, -1)
+  private var boundForChange: (minY: Double, maxY: Double) = (-1, -1)
+  private var boundForCheck: (minY: Double, maxY: Double) = (-1, -1)
   private var isChanging: Bool = false
   private var numberOfIterations: Int = 0
   private let maxNumber = 10
   private var maxValue = 999.0
   private var displayLink: CADisplayLink?
-  private var shapeLayers: [CAShapeLayer] = []
-
+  
   init(isMainPlot: Bool) {
     self.isMainPlot = isMainPlot
     super.init(frame: .zero)
     backgroundColor = .clear
   }
-
+  
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
+  
   func updateChart(_ chartRange: ChartRange) {
     self.chartRange = chartRange
     self.maxValue = isMainPlot ? chartRange.max : chartRange.allMax
-    if /*minFromChart() != boundForCheck.0 ||*/ maxFromChart() != boundForCheck.1 {
+    if /*minFromChart() != boundForCheck.0 ||*/ maxY() != boundForCheck.1 {
       updateMinMax(chartRange)
-      boundForCheck = (minFromChart(), maxFromChart())
+      boundForCheck = (minY(), maxY())
     } else if !isChanging {
       setNeedsDisplay()
     }
   }
-
-  private func minFromChart() -> Double {
+  
+  private func minY() -> Double {
     return 0.0//isMainPlot ? chartRange!.min : chartRange!.allMin
   }
-
-  private func maxFromChart() -> Double {
+  
+  private func maxY() -> Double {
     return maxValue
   }
-
+  
   private func updateMinMax(_ chartRange: ChartRange) {
     guard boundForCheck.1 != -1 else {
-      boundForChange = (minFromChart(), maxFromChart())
+      boundForChange = (minY(), maxY())
       return
     }
     isChanging = true
@@ -62,7 +61,7 @@ final class PlotView: UIView {
     displayLink = CADisplayLink(target: self, selector: #selector(update))
     displayLink?.add(to: .main, forMode: .common)
   }
-
+  
   @objc func update() {
     numberOfIterations += 1
     setNeedsDisplay()
@@ -70,7 +69,7 @@ final class PlotView: UIView {
       removeLink()
     }
   }
-
+  
   private func removeLink() {
     isChanging = numberOfIterations != maxNumber
     displayLink?.invalidate()
@@ -80,57 +79,96 @@ final class PlotView: UIView {
     numberOfIterations = 0
     boundForChange = (newMin, newMax)
   }
-
+  
+  fileprivate func drawPlot(max: Int,
+                            min: Int,
+                            context: CGContext,
+                            newValues: [CGPoint],
+                            color: CGColor) {
+    for iter in .zero...(max - min - 1) {
+      context.drawLine(
+        from: newValues[iter],
+        to: newValues[iter + 1],
+        color: color,
+        lineWidth: 1
+      )
+    }
+    context.setFillColor(UIColor.clear.cgColor)
+  }
+  
   override func draw(_ rect: CGRect) {
-    guard let context = UIGraphicsGetCurrentContext() else {
-      return
-    }
-    guard let chartRange = chartRange else {
-      return
-    }
-    let startDate = (isMainPlot ? chartRange.range.lowerBound : chartRange.xCoordinates.first!).timeIntervalSince1970
-    let endDate = (isMainPlot ? chartRange.range.upperBound : chartRange.xCoordinates.last!).timeIntervalSince1970
-    let timeFrame = endDate - startDate
+    let context = UIGraphicsGetCurrentContext()!
+    let chartRange = self.chartRange!
 
-    for axe in chartRange.activeYAxes {
-      let color = UIColor(hexString: axe.color).cgColor
-      let (width, height) = (UIScreen.main.bounds.width - 32, bounds.height)
+    let (startDate, endDate) = isMainPlot ?
+      (chartRange.range.lowerBound, chartRange.range.upperBound) :
+      (chartRange.xCoordinates.first!, chartRange.xCoordinates.last!)
+
+    let (minX, maxX) = isMainPlot ?
+      (max(0, chartRange.indicies.lowerBound - 1),
+       min(chartRange.xCoordinates.count - 1, chartRange.indicies.upperBound + 1)) :
+      (0, chartRange.xCoordinates.count - 1)
+
+    compute(isChanging: isChanging,
+            chartRange: chartRange,
+            minX: minX,
+            maxX: maxX,
+            minY: minY(),
+            maxY: maxY(),
+            size: bounds.size,
+            currentIteration: numberOfIterations,
+            maxNumberIterations: maxNumber,
+            startDate: startDate.timeIntervalSince1970,
+            endDate: endDate.timeIntervalSince1970,
+            oldBounds: boundForChange).forEach { axis in
+              drawPlot(max: maxX,
+                       min: minX,
+                       context: context,
+                       newValues: axis.points,
+                       color: axis.color)
+    }
+  }
+}
+
+func compute(isChanging: Bool,
+             chartRange: ChartRange,
+             minX: Int,
+             maxX: Int,
+             minY: Double,
+             maxY: Double,
+             size: CGSize,
+             currentIteration: Int,
+             maxNumberIterations: Int,
+             startDate: TimeInterval,
+             endDate: TimeInterval,
+             oldBounds: (minY: Double, maxY: Double)) -> [(color: CGColor,
+                                                          points: [CGPoint])] {
+    let timeFrame = endDate - startDate
+    return chartRange.activeYAxes.map { axis in
+      let color = UIColor(hexString: axis.color).cgColor
+      let (width, height) = (size.width, size.height)
       // normalize values to view coordinates
-      let min = isMainPlot ? Swift.max(0, chartRange.indicies.lowerBound - 1) : 0
-      let max = isMainPlot ? Swift.min(chartRange.xCoordinates.count - 1, chartRange.indicies.upperBound + 1) : chartRange.xCoordinates.count - 1
-      let newValues = zip(
-        chartRange.xCoordinates[min...max],
-        axe.coordinates[min...max]
-        )
+      let newValues = zip(chartRange.xCoordinates[minX...maxX], axis.coordinates[minX...maxX])
         .map { (arg) -> (CGPoint) in
           let (date, value) = arg
           // get x value from 2 to view.width - 2
           let x = ((date.timeIntervalSince1970 - startDate) / timeFrame * Double(width - 4)) + 2
-          let difference = Double(maxFromChart() - minFromChart())
+          let difference = Double(maxY - minY)
           // get y value from 0 to view.height
           let y: Double
           if isChanging { //numberOfIterations > 0 {
-            let oldDiff = boundForChange.1 - 0/*boundForChange.0*/
-            let oldY = (1 - Double(value - 0/*boundForChange.0*/) / oldDiff) * Double(height)
-            let newY = (1 - Double(value - minFromChart()) / difference) * Double(height)
-            y = oldY + (newY - oldY) * (Double(numberOfIterations) / Double(maxNumber))
+            let oldDiff = oldBounds.maxY - oldBounds.minY
+            let oldY = (1 - Double(value - oldBounds.minY) / oldDiff) * Double(height)
+            let newY = (1 - Double(value - minY) / difference) * Double(height)
+            y = oldY + (newY - oldY) * (Double(currentIteration) / Double(maxNumberIterations))
+            if value == 1130400 {
+              print(y)
+            }
           } else {
-            y = (1 - Double(value - minFromChart()) / difference) * Double(height)
+            y = (1 - Double(value - minY) / difference) * Double(height)
           }
           return CGPoint(x: x, y: y)
       }
-
-      for iter in 0...(max - min - 1) {
-        context.drawLine(
-          from: newValues[iter],
-          to: newValues[iter + 1],
-          color: color,
-          lineWidth: 1
-        )
-      }
+      return (color, newValues)
     }
-    context.setFillColor(UIColor.clear.cgColor)
-  }
 }
-
-
